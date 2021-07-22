472bfc45c3752ddd89c5ed7b2b7a8df0ea438bac
ST-AR15 <2862159026@qq.com>
2021-07-23 07:13:58 +0800

    添加了主页面poster的图片

--- src/components/poster/Poster.js
+++ src/components/poster/Poster.js
@@ -3,6 +3,11 @@ import './poster.css'
 import './poster.mobile.css'
 import { Link } from 'react-router-dom';
 
+import gamesposter from './images/poster-games.png'
+import studyposter from './images/poster-study.jpg'
+import novelposter from './images/poster-novel.jpg'
+import toolsposter from './images/poster-tools.jpg'
+
 class Poster extends React.Component {
     constructor(props) {
         super(props);
@@ -12,25 +17,33 @@ class Poster extends React.Component {
         return (
             // TODO 这里是不是不太符合H5标准？
             <ul className="poster">
-                <Link to="study" className="poster-item">
+                <Link to="study" className="poster-item" style={{
+                    backgroundImage: `url(${studyposter})`
+                }}>
                     <div className="poster-text">
                         <p>学习</p>
                         <p>Study</p>
                     </div>
                 </Link>
-                <Link to="novel" className="poster-item">
+                <Link to="novel" className="poster-item" style={{
+                    backgroundImage: `url(${novelposter})`
+                }}>
                     <div to="/novel" className="poster-text">
                         <p>小说</p>
                         <p>Novel</p>
                     </div>
                 </Link>
-                <Link to="games" className="poster-item">
+                <Link to="games" className="poster-item" style={{
+                    backgroundImage: `url(${gamesposter})`
+                }}>
                     <div className="poster-text">
                         <p>游戏</p>
                         <p>Games</p>
                     </div>
                 </Link>
-                <Link to="tools" className="poster-item">
+                <Link to="tools" className="poster-item" style={{
+                    backgroundImage: `url(${toolsposter})`
+                }}>
                     <div className="poster-text">
                         <p>工具</p>
                         <p>Tools</p>
