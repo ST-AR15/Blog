85c7e1532c71c040a7d383860f8a172f95088f0d
ST-AR15 <2862159026@qq.com>
2021-07-23 06:44:28 +0800

    完成了router的切换动画

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group'
 
 // 首页的内容
 import Title from './components/title/Title'
@@ -11,18 +12,36 @@ import Novel from './page/novel/Novel'
 import Games from './page/games/Games'
 import Tools from './page/tools/Tools'
 
+const routes = [
+    // TODO 为了适配react-router的使用习惯，另外也是为了逻辑正常，应该把Title和Poster整合起来（他们使用了同一个path）
+    { path: '/', name: 'Title', Component: Title },
+    { path: '/', name: 'Poster', Component: Poster },
+    { path: '/study', name: 'Study', Component: Study },
+    { path: '/novel', name: 'Novel', Component: Novel },
+    { path: '/games', name: 'Games', Component: Games },
+    { path: '/tools', name: 'Tools', Component: Tools },
+]
+
 function App() {
     return (
         <div className="App">
             <Router>
-                <Route path="/" exact component={ Title } />
-                <Route path="/" exact component={ Poster } />
-                <div className="stage">
-                    <Route path="/study" component={ Study } />
-                    <Route path="/novel" component={ Novel } />
-                    <Route path="/games" component={ Games } />
-                    <Route path="/tools" component={ Tools } />
-                </div>
+                { routes.map(({ path, Component }) => (
+                    <Route key={ path } exact path={ path }>
+                        {({ match }) => (
+                            <CSSTransition
+                                in={ match != null }
+                                timeout={ 600 }
+                                classNames="view"
+                                unmountOnExit
+                            >
+                                <div className="view">
+                                    <Component />
+                                </div>
+                            </CSSTransition>
+                        )}
+                    </Route>
+                ))}
 		    </Router>
         </div>
     );
